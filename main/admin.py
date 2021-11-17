26fc1812b7d5c3e01da9b147e0cdd6f02e6d6dd3
gulkayir1 <de.gulkayir@gmail.com>
2021-11-17 16:45:44 +0600

    initial commit

---
+++ main/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+
+from main.models import *
+
+class ImageInline(admin.TabularInline):
+    model = CodeImage
+    max_num = 5
+    min_num = 1
+
+
+@admin.register(Problem)
+class ProblemAdmin(admin.ModelAdmin):
+    inlines = [ImageInline,]
+
+
+admin.site.register(Reply)
+admin.site.register(Comment)
