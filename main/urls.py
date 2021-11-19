3e10738de998c184e4ac3c6542d3247b616fba65
gulkayir1 <de.gulkayir@gmail.com>
2021-11-19 16:11:29 +0600

    ViewSet, Router

---
+++ main/urls.py
@@ -0,0 +1,9 @@
+# from django.urls import path
+#
+# from .views import *
+#
+# urlpatterns = [
+#     path('problem-list/', ProblemListView.as_view()),
+#     path('problem-create/', ProblemCreateView.as_view()),
+#
+# ]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView, CreateAPIView
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from main.models import Problem, CodeImage, Reply, Comment
+from main.serializers import ProblemSerializer, ReplySerializer, CommentSerializer
+
+
+# class ProblemListView(ListAPIView):
+#     queryset = Problem.objects.all()
+#     serializer_class = ProblemListSerializer
+#
+#
+# class ProblemCreateView(CreateAPIView):
+#     queryset = Problem.objects.all()
+#     serializer_class = ProblemCreateSerializer
+#
+#
+#     def get_serializer_context(self):
+#         return {'request': self.request}
+
+class ProblemViewSet(ModelViewSet):
+    queryset = Problem.objects.all()
+    serializer_class = ProblemSerializer
+
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context['action'] = self.action
+        return context
+
+
+class ReplyViewSet(ModelViewSet):
+    queryset = Reply.objects.all()
+    serializer_class = ReplySerializer
+
+class CommentViewSet(ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
